37483edcbaa64dd74835e1adce72cbdeba897d74
Guilherme <guilhermeduarte13palhano@gmail.com>
2026-05-18 20:55:05 -0400

    feat: adiciona crud de noticias diarias

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const usersController = require('./users/');
 const tipoUsuarioController = require('./tipoUsuario/');
+const noticiasDiariasController = require('./noticias_diarias/');
 
 const routes = Router();
 
 
 routes.use('/users', usersController);
 routes.use('/tipo-usuario', tipoUsuarioController);
+routes.use('/api/noticias', noticiasDiariasController);
 
 module.exports = routes;
