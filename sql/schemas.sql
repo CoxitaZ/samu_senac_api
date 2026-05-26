d3c1b577c7f873cbad16e38be79bb68c204d52ec
Guilherme <guilhermeduarte13palhano@gmail.com>
2026-05-25 21:21:44 -0400

    Resolve conflitos

--- sql/schemas.sql
+++ sql/schemas.sql
@@ -1,24 +1,20 @@
-CREATE TABLE tipo_usuario(
-                             id_tipo SERIAL PRIMARY KEY,
-                             tipo VARCHAR(15)
+CREATE TABLE IF NOT EXISTS tipo_usuario (
+    id_tipo SERIAL PRIMARY KEY,
+    tipo VARCHAR(15)
 );
 
-CREATE TABLE usuarios(
-                         id_user SERIAL PRIMARY KEY,
-                         nome_usuario VARCHAR(100),
-                         senha TEXT,
-                         tipo int,
-                         status VARCHAR(15),
-                         FOREIGN KEY (tipo) REFERENCES tipo_usuario(id_tipo)
-                             ON UPDATE CASCADE
-                             ON DELETE RESTRICT
+CREATE TABLE IF NOT EXISTS usuarios (
+    id_user SERIAL PRIMARY KEY,
+    nome_usuario VARCHAR(100),
+    senha TEXT,
+    tipo INT,
+    status VARCHAR(15),
+    FOREIGN KEY (tipo) REFERENCES tipo_usuario(id_tipo)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
 );
 
-INSERT INTO tipo_usuario (tipo) VALUES ('user');
-INSERT INTO tipo_usuario (tipo) VALUES ('admin');
-INSERT INTO tipo_usuario (tipo) VALUES ('root');
-
-CREATE TABLE noticias (
+CREATE TABLE IF NOT EXISTS noticias (
     id_noticia SERIAL PRIMARY KEY,
     titulo VARCHAR(150),
     resumo TEXT NOT NULL,
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,16 @@
 const { Router } = require('express');
 const usersController = require('./users/');
-const tipoUsuarioController = require('./tipoUsuario/');
+const tipoUsuarioController = require('./tipousuario/');
 const noticiasDiariasController = require('./noticias_diarias/');
+const noticiasController = require('./noticias/');
 
 const routes = Router();
 
-
 routes.use('/users', usersController);
 routes.use('/tipo-usuario', tipoUsuarioController);
-routes.use('/api/noticias', noticiasDiariasController);
+
+// separar rotas 👇
+routes.use('/api/noticias', noticiasController);
+routes.use('/api/noticias-diarias', noticiasDiariasController);
 
 module.exports = routes;
